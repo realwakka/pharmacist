50a31ef40eca27b89b55412e73aea7a1dc35444a
realwakka <realwakka@gmail.com>
2014-12-06 20:36:55 +0000

    1412062036

--- app/build/generated/source/buildConfig/debug/com/realwakka/organicpractice/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/realwakka/organicpractice/BuildConfig.java
@@ -5,7 +5,7 @@ package com.realwakka.organicpractice;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.realwakka.organicpractice";
+  public static final String APPLICATION_ID = "com.realwakka.organicpractice";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
